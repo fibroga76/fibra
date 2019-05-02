062a2ecec38ab9768de950704f4d3552509fae40
fibroga76 <46776790+fibroga76@users.noreply.github.com>
2019-05-02 19:48:31 +0200

    Add files via upload

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,44 +1,42 @@
-
 import math
 
-graph = [[0,1,4,0,0,0,0,0],
-         [1,0,0,0,0,2,0,0],
-         [4,0,0,5,0,0,0,0],
-         [0,0,5,0,1,3,0,0],
-         [0,0,0,1,0,0,2,3],
-         [0,2,0,3,0,0,0,0],
-         [0,0,0,0,2,0,0,4],
-         [0,0,0,0,3,0,4,0]]
+graph = [[0,3,0,4],
+         [3,0,1,5],
+         [0,1,0,2],
+         [4,5,2,0]]
+         
 
-labelList = [math.inf] * len(graph) #inizializzazione delle label (moltiplico per il numero di nodi per le righe)
-labelList[0] = 0                    #distanza dal nodo sorgente = 0
+source = 0
+nodeNumber = len(graph)
 
-unexploredNode = [0,1,2,3,4,5,6,7]   #nodi da esplorare
+labelList = [math.inf for i in range (nodeNumber)]
+labelList[source] = 0
 
-cntPasso=0
 
-while len(unexploredNode)>0:
-    print("Passo numero: %d"% cntPasso)
-    print("Nodi inesplorati: ", unexploredNode)
-    print("Lista delle label: ", labelList)
-    print("-----------------------------------------------")
-    #scelgo il nodo con label minore(k)
-    infiniteList = [math.inf] * len(graph)
-    
+unexploredNode = [i for i in range(nodeNumber)]
+
+while len(unexploredNode)>0:       #scegliere il nodo con label minore
+
+    minLabel = min([labelList[node] for node in unexploredNode])
+
     for i in unexploredNode:
-        infiniteList[i]=labelList[i]    #valore della labelList[i] 
-    
-    minimun = min(infiniteList)   #trovo il minimo
+        if labelList[i]==minLabel:
+            currentNode = i
+        else:
+            break
 
-    if(minimun==math.inf):  #controllo per uscire dal ciclo
-        break
+
+    unexploredNode.remove(currentNode)
+
+    for neighbourNode,weight in enumerate(graph[currentNode]):   #si cicla sui vicini del sorgente (di zero), per ottenere indice e valore si aggiunge enumerate
+        if weight>0:
+            distance = labelList[currentNode]+weight
+            if(distance<labelList[neighbourNode]):
+                labelList[neighbourNode]=distance
     
-    currentNode = infiniteList.index(minimun)  #prendo l'indice del valore minimo
-    for node, weight in enumerate(graph[currentNode]):       #ciclo for con ricerca dei nodi collegati a k per indici non ancora esplorati
-        if(weight!=0):   #verifico che sia collegato e che non sia se stesso
-            newLabel = labelList[currentNode] + weight
-            if(newLabel<labelList[node]):
-               labelList[node] = newLabel     #per ogni nuovo nodo sommo (valore label(k)+peso arco di quel nodo) il valore della sua label e controllo se sono minori così sostituisco sennò niente
+
+print(labelList)
+
     
-    cntPasso=cntPasso+1
-unexploredNode.remove(currentNode) #rimuovo l'indice visitato   
+
+
